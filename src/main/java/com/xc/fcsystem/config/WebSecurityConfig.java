0248a807f48f455bce4b5a4ab92259c2f6a79d1b
Banishglory <1139691720@qq.com>
2022-03-17 22:58:07 +0800

    version 0.0.1 首页的初编写 确保前端页面资源的可引用 上传相关js文件及layui框架相关素材 添加WebMvcConfig类实现WebMvcConfigurationSupport类的方法来加载静态资源目录

--- src/main/java/com/xc/fcsystem/config/WebSecurityConfig.java
+++ src/main/java/com/xc/fcsystem/config/WebSecurityConfig.java
@@ -1,13 +1,17 @@
 package com.xc.fcsystem.config;
 
+import com.xc.fcsystem.utils.DateTimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Date;
 
 /**
  * 安全登录拦截
@@ -34,10 +38,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //解决 SpringSecurityiFrame'X-Frame-Options'to'deny'
+        System.out.println("进入securityConfig进入时间--------------------------------------------");
+        String nowTime = DateTimeUnit.formatDateD(new Date());
+        System.out.println(nowTime);
         http.headers().frameOptions().disable();
         http.csrf().disable(); //解除拦截  重要！！！！
         http.authorizeRequests()
-                .antMatchers("/js/**","/css/**","/style/**","/layui/**","/face_files/**","/images/**","/article/**","/mb/**","/code/**").permitAll()//放开资源
+                .antMatchers("/static/**","/third/**","/js/**").permitAll()//放开资源
                 .antMatchers("/template/index.html", "/index").permitAll()
                 .anyRequest().authenticated() //任何请求,登录后可以访问
                 .and()
@@ -50,8 +57,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll() //登录页面用户任意访问
                 .and()
                 .logout().permitAll(); //注销行为任意访问
+        System.out.println("securityConfig结束时间--------------------------------------------");
+        String endTime = DateTimeUnit.formatDateD(new Date());
+        System.out.println(endTime);
 
     }
 
+
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring() .antMatchers("/static/**");
+
+    }
 }
 
